921af1347d73957cdc95038c3b49cda6d1e6d58c
kohlg <kohlglenn@gmail.com>
2020-12-03 08:27:11 -0800

    Committing to push to prod

---
+++ client/build/precache-manifest.a38afa503b3bf78aec0968919ecee08c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd85a8ba3d431aabed7303c31f55a653",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aab2ff03c5102ed15574",
+    "url": "/static/css/main.d8b50328.chunk.css"
+  },
+  {
+    "revision": "f8015db5989e1e0afe6f",
+    "url": "/static/js/2.2e9457b3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2e9457b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aab2ff03c5102ed15574",
+    "url": "/static/js/main.a7859f17.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
